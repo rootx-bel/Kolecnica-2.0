c102f9cf5be9ae230dcae5bf5f69edec037985c7
Rootx <miha.vasilyev@mail.ru>
2022-09-28 19:54:15 +0300

    Настроен вход и выход из сайта

--- login/urls.py
+++ login/urls.py
@@ -2,6 +2,6 @@ from django.contrib.auth import views
 from django.urls import path
 
 urlpatterns = [
-    path('login/', views.LoginView.as_view(), name='login'),
+    path('', views.LoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 
 def index(request):
-    return HttpResponse("I hate django")
+    #return HttpResponse("I hate django")
+    return render(request, 'main/index.html')
